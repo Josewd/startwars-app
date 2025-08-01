51165d60702ef53b83b7e3c596866e99029e23e5
Josewd <exageradonome@gmail.com>
2025-08-01 14:10:00 +0100

    adding some style to text

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,12 +11,18 @@ export default function Header({ title, description }: HeaderProps) {
   return (
     <Box textAlign="center" py={2}>
         <Box display="flex" alignItems="center" justifyContent="center" gap={1} mb={2}>
-          <Public sx={{ fontSize: 32, color: '#60A5FA' }} />
-          <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold', color: 'white', fontFamily: 'Orbitron' }}>
-            {title}
+          <Public sx={{ fontSize: 32, color: 'white' }} />
+          <Typography variant="h2" component="h1" sx={{ fontWeight: 'bold',  fontFamily: 'Orbitron',
+            color: 'black',
+             '-webkit-text-stroke': '2px white',
+             textShadow: '0 0 10px white'
+          }}>
+            {title.toUpperCase()}
           </Typography>
+          <Public sx={{ fontSize: 32, color: 'white' }} />
         </Box>
-        <Typography variant="body1" sx={{ color: '#D1D5DB', maxWidth: '768px', mx: 'auto', fontFamily: 'Orbitron' }}>
+        <Typography variant="h5" sx={{ color: 'yellow',
+           maxWidth: '768px', mx: 'auto', fontFamily: 'Orbitron' }}>
           {description}
         </Typography>
       </Box>
